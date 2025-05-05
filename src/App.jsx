8d6e59ddb62bbdbe44e75b612727800c0a70c1dd
Raihanmul <raihanmul10@gmail.com>
2025-05-05 11:23:21 +0700

    update tailwindcss

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,8 @@ export default function App() {
   console.log(profiles);
   return (
     <main>
-      <h1>
-        <span>My Personal</span> <br /> Contact Book
+      <h1 className="text-2xl font-bold">
+        <span className="font-normal">My Personal</span> <br /> Contact Book
       </h1>
 
       <div className="kumpulan-kontak">
--- src/components/ProfileCard.jsx
+++ src/components/ProfileCard.jsx
@@ -1,18 +1,14 @@
 export default function ProfileCard({ name, address, phone, pfp, bestFriend }) {
   return (
     <div
-      className={`card flex flex-col border-1 border-[#EFEFEF] shadow-2xs p-px rounded-2xl gap-4 ${
+      className={`card flex flex-col border-1 border-[#EFEFEF] shadow-2xs p-5 rounded-2xl gap-4 ${
         bestFriend ? "bg-green-100 border border-green-500" : "bg-white"
       }`}
     >
       <div className="profile flex gap-4 items-center border-b-1 border-[#D3D3D3] pb-3">
         <img src={pfp} alt={name} className="w-16 rounded-full" />
         <p className="text-2xl font-medium">{name}</p>
-        {bestFriend && (
-          <span className="bg-green-700 text-white flex w-25 justify-center rounded-full">
-            Best Friend
-          </span>
-        )}
+        {bestFriend ? <p className="bg-green-700 rounded-full px-2 py-1 text-white font-medium">Best Friend</p> : ""}
       </div>
 
       <div className="flex gap-12">
